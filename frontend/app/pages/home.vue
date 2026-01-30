f619c20314c0c846382d95816ede94fa5b039206
Kenneth Ng <145921389+kennething@users.noreply.github.com>
2026-01-30 14:36:20 -0500

    friends list layout (#15)

--- frontend/app/pages/home.vue
+++ frontend/app/pages/home.vue
@@ -1,10 +1,10 @@
 <template>
-  <div v-if="isLoaded" class="flex flex-col items-start justify-start gap-6 overflow-y-scroll p-10">
+  <div ref="page" class="flex h-[calc(100dvh-3.5rem)] flex-col items-start justify-start gap-6 overflow-y-scroll p-10">
     <h3 class="text-3xl font-bold">Dashboard</h3>
 
     <div class="flex w-full items-start justify-between gap-6">
       <div class="flex grow flex-col items-center justify-center gap-6">
-        <DashboardHackathonCard v-for="hackathon in currentHackathons.slice(0, 5)" :key="hackathon.uuid" :hackathon="hackathon" />
+        <DashboardHackathonCard v-for="hackathon in loadedHackathons" :key="hackathon.uuid" :hackathon="hackathon" />
       </div>
 
       <div class="flex w-72 shrink-0 flex-col items-center justify-center gap-6">
@@ -23,7 +23,6 @@
       </div>
     </div>
   </div>
-  <div v-else class="flex flex-col items-start justify-start gap-6 overflow-y-scroll p-10"></div>
 </template>
 
 <script setup lang="ts">
@@ -36,10 +35,38 @@ definePageMeta({
 const userStore = useUserStore();
 const { currentHackathons, friends } = storeToRefs(userStore);
 
-const isLoaded = ref(false);
-onMounted(() => setTimeout(() => (isLoaded.value = true), 200));
+const page = useTemplateRef("page");
 
-// TODO: add infinite scroll for hackathons
+const loadedHackathons = ref(currentHackathons.value.slice(0, 5));
+const loadedHackathonsIndex = ref(currentHackathons.value.length <= 5 ? -1 : 5);
+
+function infiniteScrollHandler() {
+  if (!page.value || loadedHackathonsIndex.value === -1) return;
+
+  const scrollTop = page.value.scrollTop;
+  const pageHeight = page.value.clientHeight;
+  const scrollHeight = page.value.scrollHeight;
+
+  if (scrollTop + pageHeight >= scrollHeight - 100) {
+    const newIndex = loadedHackathonsIndex.value + 5;
+
+    loadedHackathons.value.push(...currentHackathons.value.slice(loadedHackathonsIndex.value, newIndex));
+    loadedHackathonsIndex.value = newIndex;
+
+    if (newIndex > currentHackathons.value.length - 1) loadedHackathonsIndex.value = -1;
+  }
+}
+const stopPageWatcher = watch(
+  page,
+  (page) => {
+    if (page) {
+      page.addEventListener("scroll", infiniteScrollHandler);
+      stopPageWatcher();
+    }
+  },
+  { immediate: true }
+);
+onUnmounted(() => page.value?.removeEventListener("scroll", infiniteScrollHandler));
 </script>
 
 <style scoped></style>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="flex flex-col items-center justify-center"></div>
+  <div class="fixed top-0 left-0 flex h-dvh w-dvw items-center justify-center">
+    <img
+      v-for="(part, i) in parts"
+      :key="part.src"
+      :src="part.src"
+      aria-hidden="true"
+      class="h-20 sm:h-36 md:h-44 lg:h-62 xl:h-80"
+      :class="{ float: animationDone, 'bounce-in': part.isVisible, 'opacity-0': !part.isVisible }"
+      :style="{ animationDelay: `${animationDone ? i * 150 : 0}ms` }"
+    />
+
+    <div class="fixed bottom-10 left-1/2 flex -translate-x-1/2 items-center justify-center gap-10">
+      <Transition>
+        <NuxtLink
+          v-show="animationDone"
+          to="/login"
+          class="rounded-full border-2 border-neutral-400/50 px-12 py-3 text-2xl font-medium transition hover:border-neutral-300 hover:bg-neutral-300 hover:text-neutral-800"
+          >Login</NuxtLink
+        >
+      </Transition>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,6 +28,85 @@ definePageMeta({
   requiresAuth: false,
   redirectIfAuth: true
 });
+
+const parts = ref([
+  {
+    src: "/logo/dev.svg",
+    isVisible: false
+  },
+  {
+    src: "/logo/dot.svg",
+    isVisible: false
+  },
+  {
+    src: "/logo/ision.svg",
+    isVisible: false
+  }
+]);
+
+const animationDone = ref(false);
+
+onMounted(async () => {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  for (let i = 0; i < parts.value.length; i++) {
+    setTimeout(() => {
+      const part = parts.value[i];
+      if (!part) return;
+
+      part.isVisible = true;
+      if (i === parts.value.length - 1) setTimeout(() => (animationDone.value = true), 1000);
+    }, i * 1000);
+  }
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+.v-enter-active {
+  transition:
+    opacity 0.5s ease,
+    transform 0.5s ease;
+}
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(5rem);
+}
+
+@keyframes bounce-in {
+  0% {
+    opacity: 0;
+    transform: translateY(100lvh);
+  }
+  70% {
+    transform: translateY(-5lvh);
+  }
+  85% {
+    transform: translateY(5lvh);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.bounce-in {
+  animation: bounce-in 1s forwards;
+}
+
+@keyframes float {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10lvh);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+.float {
+  animation: float 6s ease-in-out infinite;
+}
+</style>
